e93ab0675b9d2b3ac6c400f9ac4ca544a3b372df
Brian Friesen <brianfriesen@quickenloans.com>
2014-05-27 08:46:20 -0400

    Add NullLogger.

---
+++ Rock.Logging/NullLogger.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+
+namespace Rock.Logging
+{
+    public class NullLogger : ILogger
+    {
+        private static readonly NullLogger _instance = new NullLogger();
+
+        private readonly Task _completedTask = Task.FromResult(0);
+
+        private NullLogger()
+        {
+        }
+
+        public static NullLogger Instance
+        {
+            get { return _instance; }
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return false;
+        }
+
+        public Task Log(
+            LogEntry logEntry,
+            string callerMemberName = null,
+            string callerFilePath = null,
+            int callerLineNumber = 0)
+        {
+            return _completedTask;
+        }
+    }
+}
