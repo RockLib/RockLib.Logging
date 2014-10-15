f87e55123bfbca8c77beb2c56c3d684a3af05782
Brian Friesen <brianfriesen@quickenloans.com>
2014-10-15 10:51:03 -0400

    Add retention policy for RollingFileLogProvider.

--- Rock.Logging.IntegrationTests/LogProviders/RollingFileLogProviderTests.cs
+++ Rock.Logging.IntegrationTests/LogProviders/RollingFileLogProviderTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 using NUnit.Framework;
 using Rock.Logging;
 using Rock.Serialization;
@@ -24,28 +25,71 @@ namespace RollingFileLogProviderTests
                     _logFilePath,
                     maxFileSizeKilobytes);
 
-            LogEntry logEntry;
+            Assert.That(Directory.GetFiles(_logFileDirectory).Length, Is.EqualTo(0));
 
+            await logProvider.WriteAsync(GetLogEntry());
+            Assert.That(Directory.GetFiles(_logFileDirectory).Length, Is.EqualTo(1));
+
+            await MakeOneArchiveFile(maxFileSizeKilobytes, logProvider);
+            Assert.That(Directory.GetFiles(_logFileDirectory).Length, Is.EqualTo(2));
+        }
+
+        [Test]
+        public async void PrunesOldArchiveFiles()
+        {
+            const int maxFileSizeKilobytes = 50;
+
+            var serializer = new XmlSerializerSerializer();
+
+            var logProvider =
+                new RollingFileLogProvider(
+                    new SerializingLogFormatterFactory(serializer),
+                    _logFilePath,
+                    maxFileSizeKilobytes,
+                    2);
+
+            Assert.That(Directory.GetFiles(_logFileDirectory).Length, Is.EqualTo(0));
+
+            await logProvider.WriteAsync(GetLogEntry());
+            Assert.That(Directory.GetFiles(_logFileDirectory).Length, Is.EqualTo(1));
+
+            await MakeOneArchiveFile(maxFileSizeKilobytes, logProvider);
+            Assert.That(Directory.GetFiles(_logFileDirectory).Length, Is.EqualTo(2));
+
+            await MakeOneArchiveFile(maxFileSizeKilobytes, logProvider);
+            Assert.That(Directory.GetFiles(_logFileDirectory).Length, Is.EqualTo(3));
+
+            await MakeOneArchiveFile(maxFileSizeKilobytes, logProvider);
+            Assert.That(Directory.GetFiles(_logFileDirectory).Length, Is.EqualTo(3));
+
+            await MakeOneArchiveFile(maxFileSizeKilobytes, logProvider);
+            Assert.That(Directory.GetFiles(_logFileDirectory).Length, Is.EqualTo(3));
+        }
+
+        private static async Task MakeOneArchiveFile(int maxFileSizeKilobytes, RollingFileLogProvider logProvider)
+        {
+            var logEntry = GetLogEntry();
+
+            // Write to the log file until it has exceeded the max file size.
+            while (IsTooSmallForRollOver(maxFileSizeKilobytes))
+            {
+                await logProvider.WriteAsync(logEntry);
+            }
+
+            // This log entry should cause the existing log file to be archived, and a new one created.
+            await logProvider.WriteAsync(logEntry);
+        }
+
+        private static LogEntry GetLogEntry()
+        {
             try
             {
                 throw new Exception();
             }
             catch (Exception ex)
             {
-                logEntry = new LogEntry("Hello, world!", new { Foo = "bar", Who = "there" }, ex, "We're in a test!");
+                return new LogEntry("Hello, world!", new { Foo = "bar", Who = "there" }, ex, "We're in a test!");
             }
-
-            // Write to the log file until it has exceeded the max file size.
-            while (IsTooSmallForRollOver(maxFileSizeKilobytes))
-            {
-                await logProvider.WriteAsync(logEntry);
-                Assert.That(Directory.GetFiles(_logFileDirectory).Length, Is.EqualTo(1));
-            }
-
-            // This log entry should cause the existing log file to be archived, and a new one created.
-            await logProvider.WriteAsync(logEntry);
-
-            Assert.That(Directory.GetFiles(_logFileDirectory).Length, Is.EqualTo(2));
         }
 
         private static bool IsTooSmallForRollOver(double maxFileSizeMegabytes)
--- Rock.Logging/LogProviders/RollingFileLogProvider.cs
+++ Rock.Logging/LogProviders/RollingFileLogProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,7 +9,10 @@ namespace Rock.Logging
     public class RollingFileLogProvider : FileLogProvider
     {
         private const int _defaultMaxFileSizeKilobytes = 1024; // 1MB
+        private const int _defaultMaxArchiveCount = 10;
+
         private readonly Lazy<int> _maxFileSizeBytes;
+        private readonly Lazy<int> _maxArchiveCount;
 
         public RollingFileLogProvider(ILogFormatterFactory logFormatterFactory)
             : base(logFormatterFactory)
@@ -21,26 +25,32 @@ namespace Rock.Logging
             ILogFormatterFactory logFormatterFactory,
             string file,
             int maxFileSizeKilobytes = _defaultMaxFileSizeKilobytes,
+            int maxArchiveCount = _defaultMaxArchiveCount,
             IAsyncWaitHandle waitHandle = null)
             : base(logFormatterFactory, file, waitHandle)
         {
             MaxFileSizeKilobytes = maxFileSizeKilobytes;
+            MaxArchiveCount = maxArchiveCount;
+
             _maxFileSizeBytes = new Lazy<int>(() => GetMaxFileSizeBytes(maxFileSizeKilobytes));
+            _maxArchiveCount = new Lazy<int>(() => MaxArchiveCount);
         }
 
         public int MaxFileSizeKilobytes { get; set; }
+        public int MaxArchiveCount { get; set; }
 
         protected override Task OnPreWriteAsync(LogEntry entry, string formattedLogEntry)
         {
-            if (NeedsRollover())
+            if (NeedsArchiving())
             {
-                System.IO.File.Move(File, GetArchiveFileName());
+                ArchiveLog();
+                PruneArchives();
             }
 
             return _completedTask;
         }
 
-        private bool NeedsRollover()
+        private bool NeedsArchiving()
         {
             var fileInfo = new FileInfo(File);
             return
@@ -48,25 +58,31 @@ namespace Rock.Logging
                 && fileInfo.Length > _maxFileSizeBytes.Value;
         }
 
+        private void ArchiveLog()
+        {
+            System.IO.File.Move(File, GetArchiveFileName());
+        }
+
         private string GetArchiveFileName()
         {
-            var directory = Path.GetDirectoryName(File);
+            string directory;
+            string fileName;
+            string fileExtension;
 
-            var fileName = Path.GetFileNameWithoutExtension(File);
-            var fileExtension = Path.GetExtension(File);
-
-            var searchPattern = fileName + ".*" + fileExtension;
+            var archiveFiles = GetArchiveFiles(out directory, out fileName, out fileExtension);
 
             var archiveNumber =
-                Directory
-                    .GetFiles(directory, searchPattern)
-                    .Select(GetArchiveNumberString)
-                    .Where(IsNumber)
-                    .Select(int.Parse)
+                archiveFiles
+                    .Select(f => f.ArchiveNumber)
                     .DefaultIfEmpty()
                     .Max() + 1;
 
-            return Path.Combine(directory, fileName + "." + archiveNumber + fileExtension);
+            return Path.Combine(directory, fileName + "." + archiveNumber.ToString(GetArchiveNumberFormat()) + fileExtension);
+        }
+
+        private string GetArchiveNumberFormat()
+        {
+            return new string(Enumerable.Repeat('0', MaxArchiveCount.ToString().Length).ToArray());
         }
 
         private static string GetArchiveNumberString(string file)
@@ -75,14 +91,53 @@ namespace Rock.Logging
             return name.Substring(name.LastIndexOf('.') + 1);
         }
 
-        private static bool IsNumber(string archiveNumber)
+        private static bool IsNumber(string archiveNumberString)
         {
-            return archiveNumber.All(char.IsNumber);
+            return archiveNumberString.All(char.IsNumber);
+        }
+
+        private void PruneArchives()
+        {
+            var archiveFiles = GetArchiveFiles().OrderBy(f => f.ArchiveNumber).ToList();
+
+            while (archiveFiles.Count > _maxArchiveCount.Value)
+            {
+                System.IO.File.Delete(archiveFiles.First().File);
+                archiveFiles.RemoveAt(0);
+            }
         }
 
         private static int GetMaxFileSizeBytes(int maxFileSizeKilobytes)
         {
             return maxFileSizeKilobytes * 1024;
+        }
+
+        private IEnumerable<ArchiveFile> GetArchiveFiles()
+        {
+            string dummy1, dummy2, dummy3;
+            return GetArchiveFiles(out dummy1, out dummy2, out dummy3);
+        }
+
+        private IEnumerable<ArchiveFile> GetArchiveFiles(out string directory, out string fileName, out string fileExtension)
+        {
+            directory = Path.GetDirectoryName(File);
+
+            fileName = Path.GetFileNameWithoutExtension(File);
+            fileExtension = Path.GetExtension(File);
+
+            var searchPattern = fileName + ".*" + fileExtension;
+
+            return
+                from file in Directory.GetFiles(directory, searchPattern)
+                let archiveNumberString = GetArchiveNumberString(file)
+                where IsNumber(archiveNumberString)
+                select new ArchiveFile { File = file, ArchiveNumber = int.Parse(archiveNumberString) };
+        }
+
+        private class ArchiveFile
+        {
+            public string File { get; set; }
+            public int ArchiveNumber { get; set; }
         }
     }
 }
