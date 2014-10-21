0d0d39ab7821ca064788c17071294ac15c97720d
unknown <BFriesen@bfriesen81vm.mi.corp.rockfin.com>
2014-10-21 10:12:25 -0400

    Update nuget packages. - Rock.Core to 0.0.2

--- Rock.Logging/LogEntry.cs
+++ Rock.Logging/LogEntry.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.Xml.Serialization;
 using Rock.Defaults.Implementation;
+using Rock.Extensions;
 
 namespace Rock.Logging
 {
