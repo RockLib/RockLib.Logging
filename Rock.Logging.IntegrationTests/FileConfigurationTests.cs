94cc33bee8afc6be56243081b44680c0d11c4419
unknown <BFriesen@bfriesen81vm.mi.corp.rockfin.com>
2014-10-31 16:21:56 -0400

    Remove garbage test.

--- Rock.Logging.IntegrationTests/FileConfigurationTests.cs
+++ Rock.Logging.IntegrationTests/FileConfigurationTests.cs
@@ -1,40 +1,7 @@
-﻿using System;
-using NUnit.Framework;
-using System.Configuration;
-
-namespace Rock.Logging.IntegrationTests
+﻿namespace Rock.Logging.IntegrationTests
 {
     public class FileConfigurationTests
     {
-        [Test]
-        public void Foo()
-        {
-            var factory = (ILoggerFactory)ConfigurationManager.GetSection("rock.logging");
-            var logger = factory.Get<Logger>();
-            logger.Warn(GetLogEntry());
-        }
-
-        private static LogEntry GetLogEntry()
-        {
-            Exception exception;
-
-            try
-            {
-                try
-                {
-                    throw new Exception();
-                }
-                catch (Exception ex)
-                {
-                    throw new InvalidOperationException("o noes!", ex);
-                }
-            }
-            catch (Exception ex)
-            {
-                exception = ex;
-            }
-
-            return new LogEntry("Hello, world!", new { foo = "you <> me", baz = "qux" }, exception, "testing...");
-        }
+        
     }
 }
