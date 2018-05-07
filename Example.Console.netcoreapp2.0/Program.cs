ab3458fc66c44ad9351c47f83a774a9cdc96daf4
Brian Friesen <brianfriesen@quickenloans.com>
2018-05-07 11:14:28 -0400

    Add example project

---
+++ Example.Console.netcoreapp2.0/Program.cs
@@ -0,0 +1,23 @@
+﻿using RockLib.Logging;
+using System;
+
+namespace Example
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Logger logger = LoggerFactory.GetInstance())
+            {
+                var service = new ExampleService(logger);
+
+                service.DoSomethingDangerous(123, "abc");
+                service.DoSomethingDangerous(42, "What do you mean? African or European swallow?");
+            }
+
+            Console.WriteLine();
+            Console.Write("Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
