a45b0c87cb44b780f2c070dab0700ed71689b10a
Brian Friesen <brianfriesen@quickenloans.com>
2018-06-15 14:11:15 -0400

    Add asp.net core 2.0 example project

---
+++ Example.AspNetCore.netcoreapp2.0/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RockLib.Configuration;
+using RockLib.Logging;
+using RockLib.Logging.AspNetCore;
+
+namespace Example.AspNetCore.netcoreapp2._0
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webHost = BuildWebHost(args);
+            
+            try
+            {
+                webHost.Run();
+            }
+            finally
+            {
+                LoggerFactory.ShutDown();
+            }
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseRockLibLogging()
+                .Build();
+    }
+}
