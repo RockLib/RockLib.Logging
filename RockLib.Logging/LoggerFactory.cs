c39dc027547eb7a3e1daac20dd24069b242acc1b
Brian Friesen <brianfriesen@quickenloans.com>
2018-08-03 13:04:42 -0400

    Add LoggerFactory.CreateFromConfig method

--- RockLib.Logging/LoggerFactory.cs
+++ RockLib.Logging/LoggerFactory.cs
@@ -23,6 +23,11 @@ namespace RockLib.Logging
     /// </remarks>
     public static class LoggerFactory
     {
+        /// <summary>
+        /// The section name, relative to <see cref="Config.Root"/>, from which to retrieve logging settings.
+        /// </summary>
+        public const string SectionName = "rocklib.logging";
+
         private static readonly ConcurrentDictionary<string, Logger> _lookup = new ConcurrentDictionary<string, Logger>(StringComparer.InvariantCultureIgnoreCase);
 
         private static readonly Semimutable<IReadOnlyCollection<Logger>> _loggers = new Semimutable<IReadOnlyCollection<Logger>>(CreateDefaultLoggers);
@@ -54,6 +59,35 @@ namespace RockLib.Logging
         public static Logger GetInstance(string name = Logger.DefaultName) => _lookup.GetOrAdd(name ?? Logger.DefaultName, FindLogger);
 
         /// <summary>
+        /// Creates a new instance of the <see cref="Logger"/> class using the configuration found in the
+        /// section named <see cref="SectionName"/> in <see cref="Config.Root"/> according to the specified name.
+        /// </summary>
+        /// <param name="name">The name of the logger to create, as matched in configuration.</param>
+        /// <returns>A new instance of the <see cref="Logger"/> class.</returns>
+        /// <remarks>
+        /// The logger returned from this method is *not* cached. That is, a new instance is returned every time this method is called.
+        /// </remarks>
+        public static Logger CreateFromConfig(string name = Logger.DefaultName)
+        {
+            var loggingSection = Config.Root.GetSection(SectionName);
+            if (loggingSection.GetChildren().Any(c => int.TryParse(c.Key, out var dummy)))
+            {
+                foreach (var child in loggingSection.GetChildren())
+                    if ((child[nameof(Logger.Name)] ?? Logger.DefaultName) == name)
+                        return child.Create<Logger>();
+
+                throw new KeyNotFoundException($"The {SectionName} section in RockLib.Configuration.Config.Root does not contain a Logger configuration with the name '{name}'.");
+            }
+            else
+            {
+                if ((loggingSection[nameof(Logger.Name)] ?? Logger.DefaultName) == name)
+                    return loggingSection.Create<Logger>();
+
+                throw new KeyNotFoundException($"The {SectionName} section in RockLib.Configuration.Config.Root does not contain a Logger configuration with the name '{name}'.");
+            }
+        }
+
+        /// <summary>
         /// Calls the <see cref="Logger.Dispose"/> method on each logger in the <see cref="Loggers"/> property,
         /// blocking until any of their pending logging operations have completed.
         /// </summary>
@@ -72,6 +106,6 @@ namespace RockLib.Logging
         /// </summary>
         /// <returns>A collection of new <see cref="Logger"/> objects.</returns>
         public static IReadOnlyCollection<Logger> CreateDefaultLoggers() =>
-             Config.Root.GetSection("rocklib.logging").Create<IReadOnlyCollection<Logger>>();
+             Config.Root.GetSection(SectionName).Create<IReadOnlyCollection<Logger>>();
     }
 }
