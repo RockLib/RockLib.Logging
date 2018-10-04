a2cf724471e45a0f13cd56fe93803340bebbf208
Justin Moss <Justin.Moss@gmail.com>
2018-10-04 16:30:16 -0400

    Implement synchronous logging for short lived apps

---
+++ RockLib.Logging/Sync.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RockLib.Logging
+{
+    internal static class Sync
+    {
+        public static void OverAsync(Func<Task> getTaskOfTResult)
+        {
+            SynchronizationContext old = SynchronizationContext.Current;
+            try
+            {
+                SynchronizationContext.SetSynchronizationContext(null);
+                getTaskOfTResult().GetAwaiter().GetResult();
+            }
+            finally
+            {
+                SynchronizationContext.SetSynchronizationContext(old);
+            }
+        }
+    }
+}
