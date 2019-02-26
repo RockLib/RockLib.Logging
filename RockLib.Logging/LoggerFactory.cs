98874ee58a9cd1374f52a74ebc57f5e487128107
Brian Friesen <brianfriesen@quickenloans.com>
2019-02-26 16:38:40 -0500

    Fix compiler warnings in doc comments

--- RockLib.Logging/LoggerFactory.cs
+++ RockLib.Logging/LoggerFactory.cs
@@ -7,6 +7,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using Resolver = RockLib.Configuration.ObjectFactory.Resolver;
 
 namespace RockLib.Logging
 {
