2b69b86c17862f6c27de9f9ecd1d2034e441da96
Brian Friesen <brianfriesen@quickenloans.com>
2019-04-24 15:07:59 -0400

    Remove unused build directory

--- build/buildNuget.bat
+++
@@ -1,6 +0,0 @@
-
-nuget restore -SolutionDirectory ../  ../Rock.Logging/Rock.Logging.csproj
-
-msbuild /p:Configuration=Release /t:Clean;Rebuild ..\Rock.Logging\Rock.Logging.csproj
-
-nuget pack ..\Rock.Logging\Rock.Logging.csproj -Properties Configuration=Release
