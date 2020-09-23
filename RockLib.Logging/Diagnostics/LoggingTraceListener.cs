b72c31ccd5f5bf720bd58f3703a3e4a40feb7162
Brian Friesen <brianfriesen@quickenloans.com>
2020-09-23 12:22:37 -0400

    Add LoggingTraceListener
    Co-authored-by: Justin Moss <justin.moss@gmail.com>

---
+++ RockLib.Logging/Diagnostics/LoggingTraceListener.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace RockLib.Logging.Diagnostics
+{
+    using static Logger;
+
+    /// <summary>
+    /// A trace listener that outputs to an <see cref="ILogger"/>.
+    /// </summary>
+    public class LoggingTraceListener : TraceListener
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoggingTraceListener"/> class.
+        /// </summary>
+        /// <param name="logger">The logger that will record trace messages.</param>
+        /// <param name="logLevel">The level that trace messages will be logged at.</param>
+        public LoggingTraceListener(ILogger logger, LogLevel? logLevel = null)
+            : base(logger?.Name ?? DefaultName)
+        {
+            Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            LogLevel = logLevel ?? logger.Level;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoggingTraceListener"/> class.
+        /// </summary>
+        /// <param name="loggerName">
+        /// The name of the logger that will record trace messages.
+        /// <para>This logger is created by calling <see cref="LoggerFactory.Create"/>.</para>
+        /// </param>
+        /// <param name="logLevel">The level that trace messages will be logged at.</param>
+        public LoggingTraceListener(string loggerName = DefaultName, LogLevel? logLevel = null)
+            : this(LoggerFactory.Create(loggerName ?? DefaultName), logLevel)
+        {
+        }
+
+        /// <summary>
+        /// The logger that records trace messages.
+        /// </summary>
+        public ILogger Logger { get; }
+
+        /// <summary>
+        /// The level that trace messages are logged at.
+        /// </summary>
+        public LogLevel LogLevel { get; }
+
+        /// <summary>
+        /// Writes the specified message to <see cref="Logger"/>.
+        /// </summary>
+        /// <param name="message">The message to write.</param>
+        public override void Write(string message) => WriteLog(message);
+
+        /// <summary>
+        /// Writes the specified message to <see cref="Logger"/>.
+        /// </summary>
+        /// <param name="message">The message to write.</param>
+        public override void WriteLine(string message) => WriteLog(message);
+
+        private void WriteLog(string message)
+        {
+            if (Logger.IsEnabled(LogLevel))
+                Logger.Log(new LogEntry(message, LogLevel));
+        }
+    }
+}
