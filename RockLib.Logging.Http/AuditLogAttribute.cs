f0f03df63c9aca0062fd94e549051a969fab3c1a
Brian Friesen <brianfriesen@quickenloans.com>
2021-01-05 16:44:32 -0500

    Add doc comments

--- RockLib.Logging.Http/AuditLogAttribute.cs
+++ RockLib.Logging.Http/AuditLogAttribute.cs
@@ -1,7 +1,18 @@
 ﻿namespace RockLib.Logging.Http
 {
+    /// <summary>
+    /// An action filter that records an audit log for each request.
+    /// </summary>
     public class AuditLogAttribute : LoggingActionFilter
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AuditLogAttribute"/> class.
+        /// </summary>
+        /// <param name="messageFormat">
+        /// The message format string. The action name is used as the <c>{0}</c> placeholder when
+        /// formatting the message.
+        /// </param>
+        /// <param name="loggerName">The name of the logger.</param>
         public AuditLogAttribute(string messageFormat = DefaultMessageFormat, string loggerName = Logger.DefaultName)
             : base(messageFormat, loggerName, LogLevel.Audit)
         {
--- RockLib.Logging.Http/LoggingActionFilter.cs
+++ RockLib.Logging.Http/LoggingActionFilter.cs
@@ -6,10 +6,25 @@ using System;
 
 namespace RockLib.Logging.Http
 {
+    /// <summary>
+    /// An action filter that records a log for each request.
+    /// </summary>
     public abstract class LoggingActionFilter : Attribute, IActionFilter
     {
+        /// <summary>
+        /// The default message format string.
+        /// </summary>
         public const string DefaultMessageFormat = "Request handled by {0}.";
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoggingActionFilter"/> class.
+        /// </summary>
+        /// <param name="messageFormat">
+        /// The message format string. The action name is used as the <c>{0}</c> placeholder when
+        /// formatting the message.
+        /// </param>
+        /// <param name="loggerName">The name of the logger.</param>
+        /// <param name="logLevel">The level to log at.</param>
         protected LoggingActionFilter(string messageFormat, string loggerName, LogLevel logLevel)
         {
             MessageFormat = string.IsNullOrEmpty(messageFormat)
@@ -19,13 +34,23 @@ namespace RockLib.Logging.Http
             LogLevel = logLevel;
         }
 
+        /// <summary>
+        /// Gets the message format string. The action name is used as the <c>{0}</c> placeholder
+        /// when formatting the message.
+        /// </summary>
         public string MessageFormat { get; }
 
+        /// <summary>
+        /// Gets the name of the logger.
+        /// </summary>
         public string LoggerName { get; }
 
+        /// <summary>
+        /// Gets the level to log at.
+        /// </summary>
         public LogLevel LogLevel { get; }
 
-        public void OnActionExecuting(ActionExecutingContext context)
+        void IActionFilter.OnActionExecuting(ActionExecutingContext context)
         {
             var loggerLookup = context.HttpContext.RequestServices.GetRequiredService<LoggerLookup>();
             var logger = loggerLookup(LoggerName);
@@ -36,7 +61,7 @@ namespace RockLib.Logging.Http
             SetLoggingContext(context, logger, logEntry);
         }
 
-        public void OnActionExecuted(ActionExecutedContext context)
+        void IActionFilter.OnActionExecuted(ActionExecutedContext context)
         {
             var (logger, logEntry) = GetLoggingContext(context);
 
