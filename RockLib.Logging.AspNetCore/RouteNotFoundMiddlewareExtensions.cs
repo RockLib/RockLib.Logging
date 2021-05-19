f701058cbf732a1e3b415493708a2a3deb0992dd
Justin Moss <justinmoss@quickenloans.com>
2021-05-19 16:38:26 -0400

    Add RouteNotFoundMiddleware and extension

---
+++ RockLib.Logging.AspNetCore/RouteNotFoundMiddlewareExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace RockLib.Logging.AspNetCore
+{
+    /// <summary>
+    /// Provides extension methods for the <see cref="RouteNotFoundMiddleware"/> class.
+    /// </summary>
+    public static class RouteNotFoundMiddlewareExtensions
+    {
+        /// <summary>
+        /// Adds <see cref="RouteNotFoundMiddleware"/> to the application's request pipeline.
+        /// </summary>
+        /// <param name="builder">The <see cref="IApplicationBuilder"/>.</param>
+        /// <returns>The <see cref="IApplicationBuilder"/>.</returns>
+        public static IApplicationBuilder UseRouteNotFoundLogging(this IApplicationBuilder builder) =>
+            builder.UseMiddleware<RouteNotFoundMiddleware>();
+    }
+}
---
+++ RockLib.Logging.AspNetCore/RouteNotFoundMiddlewareOptions.cs
@@ -0,0 +1,23 @@
+﻿namespace RockLib.Logging.AspNetCore
+{
+    /// <summary>
+    /// Defines the options for the <see cref="RouteNotFoundMiddleware"/> class.
+    /// </summary>
+    public class RouteNotFoundMiddlewareOptions
+    {
+        /// <summary>
+        /// Gets or sets the name of the logger.
+        /// </summary>
+        public string LoggerName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the level used when sending logs.
+        /// </summary>
+        public LogLevel LogLevel { get; set; }
+
+        /// <summary>
+        /// Gets or sets the message used when sending logs.
+        /// </summary>
+        public string LogMessage { get; set; }
+    }
+}
