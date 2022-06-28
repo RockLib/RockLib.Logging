87cb6f102b24def30b295ef59ce26dea57c9f59a
Jerry King <jerryking@rocketmortgage.com>
2022-06-28 15:19:09 -0400

    Add an ILogLevelResolver; fix an issue with not disposing of a cancellationtokensource

---
+++ RockLib.Logging/LogProviders/ILogLevelResolver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockLib.Logging.LogProviders
+{
+    /// <summary>
+    /// Used to resolve the <see cref="LogLevel"/> when logging with an <see cref="ILogger"/>
+    /// </summary>
+    public interface ILogLevelResolver
+    {
+        /// <summary>
+        /// Retrieves the currently active <see cref="LogLevel"/>
+        /// </summary>
+        /// <returns>The current <see cref="LogLevel"/></returns>
+        LogLevel GetLogLevel();
+    }
+}
